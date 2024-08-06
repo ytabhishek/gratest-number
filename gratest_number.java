638eddd7106a38a5ed84b8d424203e3ff137c652
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 23:57:39 +0530

    Add files via upload

---
+++ gratest_number.java
@@ -0,0 +1,21 @@
+package conditional;
+import java.util.Scanner;
+public class gratest_number {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.println("Enter the third number: ");
+        int num3 = sc.nextInt();
+
+        if(num1>num2 && num1>num3){
+            System.out.println("num1 is gratest");
+        }else if(num2>num1 && num2>num3){
+            System.out.println("num2 is gratest");
+        }else {
+            System.out.println("num3 is gratest");
+        }
+    }
+}
